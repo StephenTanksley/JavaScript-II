cb049bf5cf3aa77917f2c097c9eac1b338b87d15
Stephen Tanksley <stephen.tanksley@gmail.com>
2019-07-08 14:58:16 -0500

    Nearly finished with homework problems after the break. git status

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,30 +1,23 @@
 // Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
 
-// const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
-/* 
 
   //Given this problem: 
   
   function firstItem(arr, cb) {
     // firstItem passes the first item of the given array to the callback function.
   }
-
   // Potential Solution:
-
   // Higher order function using "cb" as the call back
   function firstItem(arr, cb) {
     return cb(arr[0]);
   }
-
   // Function invocation 
   firstItem(items, function(first) {
     console.log(first)
   });
 
-*/
-
-const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   cb(arr.length);
@@ -73,15 +66,49 @@ multiplyNums(2, 5, (product) => {
 });
 
 
+function contains(item, list, cb) {
+  // contains checks if an item is present inside of the given array/list.
+  // Pass true to the callback if it is, otherwise pass false.
+  
+  if(list.includes(item)){
+    cb(true)
+  } else {
+    cb(false)
+  }
+}
+
+contains('Pencil', items, function(search) {
+  console.log(search);
+});
+
+
+
+//ADDITIONAL PRACTICE WRITING OUT CALLBACKS. NOT RELATED TO THE ASSIGNMENT//
+
+
+// const tattooShop = ['ink', 'tattoo gun', 'chairs', 'TV'];
+
 // function contains(item, list, cb) {
-//   // contains checks if an item is present inside of the given array/list.
-//   // Pass true to the callback if it is, otherwise pass false.
+//   if(list.includes(item)){
+//     cb(true);
+//   }else {
+//     cb(false);
+//   }
 // }
+
+// contains('tattoo gun', tattooShop, function(search) {
+//   console.log(search);
+// })
+
+
 
 // /* STRETCH PROBLEM */
+let duplicate = [5, 5, 3, 6, 2, 8, 2];
 
-// function removeDuplicates(array, cb) {
-//   // removeDuplicates removes all duplicate values from the given array.
-//   // Pass the duplicate free array to the callback function.
-//   // Do not mutate the original array.
-// }
+function removeDuplicates(array, cb) {
+  // removeDuplicates removes all duplicate values from the given array.
+  // Pass the duplicate free array to the callback function.
+  // Do not mutate the original array.
+  let dupeFree = [];
+  
+}
